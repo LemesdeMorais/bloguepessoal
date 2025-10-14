da39abb4af432adb83bbd7e4a69fa355acfdb29b
Rafaela Lemes <r.lemesdemorais@gmail.com>
2025-10-14 16:52:41 -0300

    Implementação CRUD

--- src/main/java/com/generation/bloguepessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/bloguepessoal/repository/PostagemRepository.java
@@ -1,11 +1,15 @@
 package com.generation.bloguepessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generation.bloguepessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 	
+	//select * from tb_postagens where titulo like "%?%";
+	public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 	
 	
 	
